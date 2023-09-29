31f6b03231909ed652e5c711cf64a49b1b29a75d
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2023-09-29 15:01:55 -0600

    Update ThemeSwitcher.tsx

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -19,7 +19,7 @@ export const ThemeSwitcher = () => {
     <button
       className={`bg-stone-200 hover:bg-stone-300 p-2 rounded text-stone-600 whitespace-nowrap dark:bg-gray-900 dark:text-gray-50 dark:hover:bg-gray-950`}
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
-      {theme === 'light' ? '🌙 Dark Mode' : '☀️ Light Mode'}
+      {theme === 'light' ? '🌘 Dark Mode' : '☀️ Light Mode'}
     </button>
   );
 };
