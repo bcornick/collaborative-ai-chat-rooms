da787a950ba4f805ee33d06392fecf8db6a170b3
Brett Cornick <31320688+bcornick@users.noreply.github.com>
2023-09-29 12:58:46 -0600

    fixing auth

--- app/api/session/route.ts
+++ app/api/session/route.ts
@@ -1,17 +1,19 @@
-import { NextRequest, NextResponse } from "next/server";
-import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
 
-const secret = process.env.NEXTAUTH_SECRET;
+// const secret = process.env.NEXTAUTH_SECRET;
 
 export async function GET(req: NextRequest) {
-  const sessionToken = await getToken({ req, secret, raw: true });
+  // const sessionToken = await getToken({ req, secret, raw: true });
+  const sessionToken = await getToken({ req, raw: true });
   if (!sessionToken) {
-    return NextResponse.json({ error: "No token" }, { status: 401 });
+    return NextResponse.json({ error: 'No token' }, { status: 401 });
   }
 
-  const session = await getToken({ req, secret });
+  // const session = await getToken({ req, secret });
+  const session = await getToken({ req });
   if (!session) {
-    return NextResponse.json({ error: "No session" }, { status: 401 });
+    return NextResponse.json({ error: 'No session' }, { status: 401 });
   }
 
   return NextResponse.json({
